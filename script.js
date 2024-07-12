acb1420d4f15c686ad7098cc1064e4562a931177
Rachel Pizane <rachel.pzm@gmail.com>
2024-07-12 10:57:50 -0300

    Listei os temas e cinco palavras para cada um

---
+++ script.js
@@ -0,0 +1,5 @@
+const tema = ["Animais", "Frutas", "Países"];
+
+const animais = ["cachorro", "gato", "elefante", "girafa", "tigre"];
+const frutas = ["maçã", "banana", "laranja", "uva", "morango"];
+const paises = ["Brasil", "Estados Unidos", "França", "Japão", "Austrália"];
