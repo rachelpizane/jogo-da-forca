74ef7fa4c1c82faf0d9f1d76e57b61a65a3098ac
Rachel Pizane <rachel.pzm@gmail.com>
2024-07-30 06:41:37 -0300

    feat: criei function para atualizar o hangman a cada erro

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ palavras.push(animais, frutas, paises);
 
 let palavraEscolhida;
 let palavraPreenchida;
+let tentativas;
 
 // Função para traçar o título principal
 function tracarTituloPrincipal() {
@@ -32,6 +33,44 @@ function tracarTituloPrincipal() {
   });
 }
 
+// Função para inserir a imagem do hangman
+function inserirImagemHangman(){
+  const hangmanContainer = document.getElementById("hangman-container")
+  
+  hangmanContainer.innerHTML = `
+    <svg width="199" height="227" viewBox="0 0 199 227" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g id="Image Hangman Game">
+    <g id="hangman-2" >
+    <path id="left-arm" class="game-page__hangman-game" d="M124.488 103L159.527 123.23" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <path id="right-arm" class="game-page__hangman-game" d="M159.488 123.23L194.527 103" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <path id="left-leg" class="game-page__hangman-game" d="M133.488 189.999L159.489 159" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <path id="right-leg" class="game-page__hangman-game" d="M159.488 159L185.488 190" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <path id="body" class="game-page__hangman-game" d="M159.488 107V159" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <circle id="head" class="game-page__hangman-game" cx="159.488" cy="80" r="23.5" stroke="#23291F" stroke-width="9"/>
+    </g>
+    <g id="hangman-1">
+    <path id="Line 7" d="M4.47296 222.742H194.491" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <path id="Line 8" d="M36.4879 222L36.4879 4" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <path id="Line 9" d="M159.488 4L36.4878 4" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <path id="Line 12" d="M163.488 4C163.488 1.79086 161.697 0 159.488 0C157.279 0 155.488 1.79086 155.488 4L163.488 4ZM155.488 52V56H163.488V52H155.488ZM155.488 4V52H163.488V4L155.488 4Z" fill="#23291F"/>
+    <path id="Line 11" d="M36.4879 4L61.4879 27" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    <path id="Line 10" d="M87.4878 4L36.4878 54" stroke="#23291F" stroke-width="8" stroke-linecap="round"/>
+    </g>
+    </g>
+    </svg>`
+
+  const hangman = hangmanContainer.querySelectorAll(".game-page__hangman-game");
+ 
+  hangman.forEach(part => {
+    console.log(part)
+    part.classList.toggle("hangman-game--visibility")
+  })
+}
+
+// Em andamento
+function atualizarImagemHangman(){
+  
+}
 // Função para gerar um número aleatório
 function gerarNumeroAleatorio(largura) {
   return Math.floor(Math.random() * largura);
@@ -45,23 +84,23 @@ function gerarTema() {
 /* Não to satisfeita, acho que é possivel transformar isso em um objeto, pensar nisso.*/
 // Função para gerar uma palavra aleatória
 function gerarPalavraEscolhida() {
-  const indexTema = gerarTema();
-  const indexPalavra = gerarNumeroAleatorio(palavras[indexTema].length);
+  const indexTema = gerarTema(); //Gera um número aleatório para escolher o tema
+  const indexPalavra = gerarNumeroAleatorio(palavras[indexTema].length); // Gera um número aleatório para escolher a palavra
 
   console.log(temas[indexTema]) //Auxilio, excluir depois
   console.log(palavras[indexTema][indexPalavra]) //Auxilio, excluir depois
 
-  palavraEscolhida = palavras[indexTema][indexPalavra].split("");
+  palavraEscolhida = palavras[indexTema][indexPalavra].split(""); //Transforma a palavra escolhida aleatoriamente em um array
 }
 
-// Função para gerar os traços da palavra escolhida
+// Função para gerar os traços sem nenhum caracter preenchido da palavra escolhida
 function gerarTracoPalavraEscolhida(){
   return palavraEscolhida.map(letra => {
     if(letra !== " "){
       return null;
     }
     return letra;
-  });
+  }); 
 }
 
 // Função para validar a tecla selecionada
@@ -70,14 +109,15 @@ function validarTeclaSelecionada(teclaSelecionada){
     console.log("OK")  //Auxilio, excluir depois
 
     palavraEscolhida.forEach((letra, index) => {
-      if(letra === teclaSelecionada){
-        palavraPreenchida[index] = letra;
+      if(letra === teclaSelecionada){ 
+        palavraPreenchida[index] = letra; //Preenche a palavra escolhida com a letra correta
       }
-    })
+    }) 
     console.log(palavraPreenchida);  //Auxilio, excluir depois
     atualizarTracoPalavraEscolhida() 
   } else{
     console.log("Errou...")  //Auxilio, excluir depois
+    atualizarImagemHangman();
   }
 }
 
@@ -113,6 +153,8 @@ function gerarTecladoVirtual(teclado){
     containerLetra.innerHTML = letra;
     teclado.appendChild(containerLetra)
   })
+
+  capturarTecla(teclado);
 }
 
 // Função para capturar a tecla selecionada
@@ -136,9 +178,10 @@ function iniciarJogo() {
 
   homePage.classList.toggle("hide-page"); // Esconde a página inicial
 
-  gerarPalavraEscolhida(); 
-  palavraPreenchida = gerarTracoPalavraEscolhida();
-  atualizarTracoPalavraEscolhida()
+  tentativas = 6;
+  gerarPalavraEscolhida(); // Gera a palavra escolhida aleatoriamente
+  palavraPreenchida = gerarTracoPalavraEscolhida(); // Inicializa com a palavra vazia
+  atualizarTracoPalavraEscolhida() 
 }
 
 // Função para atualizar o conteúdo da página
@@ -150,5 +193,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("btn").addEventListener("click", iniciarJogo);
 
   gerarTecladoVirtual(teclado) 
-  capturarTecla(teclado);
+  
+
+  inserirImagemHangman();
 });
